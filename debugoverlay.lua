c017011723dbeaaf1430dbe6393774998ba482c1
Westerbly Snaydley <westerbly@snaydley.com>
2015-10-29 16:26:30 -0500

    debug overlay header

--- debugoverlay.lua
+++ debugoverlay.lua
@@ -1,3 +1,12 @@
+--[[
+Copyright (c) 2015, Westerbly (radgeRayden) Snaydley
+This is licensed under the MIT license (http://opensource.org/licenses/MIT).
+Time of creation: September 9th, 2015 20:59 ACT
+Description:
+Simple overlay for displaying debug information (only fps as of this version).
+Version: 1.00
+--]]
+
 overlay = {}
 
 local fps = 0
